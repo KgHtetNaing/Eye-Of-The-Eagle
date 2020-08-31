5c582aea3b095b3b4b90afe2eb7fc6f232213e92
KgHtetNaing <noel.kghtet.99@gmail.com>
2020-08-31 21:19:20 +0630

    view folders

---
+++ resources/views/backend/hotel/list.blade.php
@@ -0,0 +1,52 @@
+<x-backend>
+   
+
+    <div class="container-fluid mt--6">
+      
+      <div class="row" style="margin-top: 180px">
+        <div class="col-xl-8">
+          <div class="card">
+            <div class="card-header border-0">
+              <div class="row align-items-center">
+                <div class="col">
+                  <h3 class="mb-0">Available Hotels</h3>
+                </div>
+                <div class="col text-right tableIcon">
+                 <a href="#" class="plusBtn" style="color: black;" >
+                  <i class="icofont-plus-circle"></i>
+                </a>
+                </div>
+              </div>
+            </div>
+            <div class="table-responsive">
+              <!-- Projects table -->
+              <table class="table align-items-center table-flush">
+                <thead class="thead-light">
+                  <tr>
+                    <th scope="col">Hotel </th>
+                    <th scope="col">Hotel Photo </th>
+                    
+
+                    
+
+                    <th scope="col">Price</th>
+                    
+                    
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <th scope="col">Hotel Name </th>          
+                    <th scope="col"><img src="" alt="hotel photo"> </th>          
+                          
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          
+        </div>
+       
+    </div>
+  </div>
+</x-backend>      
